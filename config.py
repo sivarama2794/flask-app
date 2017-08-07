ec3c4b94d38f2f6308cf402d9c1e65a4fe97a121
sivarama2794 <sivarama2794@gmail.com>
2017-08-06 20:32:32 -0400

    Main push

---
+++ config.py
@@ -0,0 +1,5 @@
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flask1app:flask-app@flaskapp.cxhp6zu7sex8.us-east-2.rds.amazonaws.com:3306/flask1app'
+SQLALCHEMY_POOL_RECYCLE = 3600
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'THoQ45DJVSUGgsZ93uB1W/AR56CiuumzAPTDS4Zy'
